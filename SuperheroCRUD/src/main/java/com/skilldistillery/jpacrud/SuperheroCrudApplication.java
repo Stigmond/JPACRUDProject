9f763af357d079bd874e5cceab3b601990a254e5
Stigmond <stigmond@gmail.com>
2020-10-26 10:13:15 -0600

    WAR deployment configuration

--- SuperheroCRUD/src/main/java/com/skilldistillery/jpacrud/SuperheroCrudApplication.java
+++ SuperheroCRUD/src/main/java/com/skilldistillery/jpacrud/SuperheroCrudApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.jpacrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SuperheroCrudApplication {
+public class SuperheroCrudApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(SuperheroCrudApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperheroCrudApplication.class, args);
